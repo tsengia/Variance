c4eada06d1a0ec5a23ea4e747a89a5d4294b2929
ExpandingDev <javscripter@yahoo.com>
2020-12-21 23:56:16 -0500

    Adding unit tests

--- test/test_nutrient.py
+++ test/test_nutrient.py
@@ -11,3 +11,14 @@ class MacroNutrientTest(TestCase):
 
     def test_calorie_conversion(self):
         self.assertEqual(MacroNutrient.calories_per_measurement(MacroNutrient.FAT, Grams(2)), Calories(18))
+        with self.assertRaises(TypeError):
+            MacroNutrient.calories_per_measurement(MacroNutrient.PROTEIN, "asdfg")
+        with self.assertRaises(TypeError):
+            MacroNutrient.calories_per_measurement(8, Grams(2))
+
+    def test_nutrient_target_init(self):
+        n = NutrientTarget()
+        self.assertEqual(n.calories, Calories(0))
+        self.assertEqual(n.protein, Grams(0))
+        self.assertEqual(n.carbohydrates, Grams(0))
+        self.assertEqual(n.fats, Grams(0))
