90ff500e31f4b97b694c178ae010b1508dd066ce
ExpandingDev <javscripter@yahoo.com>
2021-01-01 22:39:46 -0500

    Adding remaining auth functions

--- variance/auth.py
+++ variance/auth.py
@@ -52,3 +52,26 @@ def register():
     user = db.execute("SELECT * FROM UserIndex WHERE username = ?", (username,)).fetchone()
 
     return { "uid":str(user["id"]) }
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return { "message":"Logged out." }
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute("SELECT * FROM UserIndex WHERE id=?", (user_id,))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return {"error":"You must be logged in to access this endpoint!"}
+        return view(**kwargs)
+
+    return wrapped_view
