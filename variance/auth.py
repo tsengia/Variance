36594705fc6ca93346923ac4ca09f6dd43b3b697
ExpandingDev <javscripter@yahoo.com>
2021-01-01 20:58:40 -0500

    Added registration to auth

---
+++ variance/auth.py
@@ -0,0 +1,39 @@
+import functools
+import datetime
+
+from flask import (
+    Blueprint, flash, g, redirect, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from variance.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route("/register", methods=("POST"))
+def register():
+    if request_method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        birthday = request.form["birthday"]
+        db = get_db()
+
+        if not username:
+            return {"error":"You must specify a username!"}
+        elif not password:
+            return {"error":"You must specify a password!"}
+        elif not birthday:
+            return {"error":"You must specify a birthday!"}
+        try:
+            birthday = datetime.date.fromisoformat(birthday)
+        except ValueError:
+            return {"error":"Birthday is not in a valid YYYY-MM-DD format!"}
+            
+        elif db.execute("SELECT id FROM UserIndex WHERE username = ?", (username,)).fetchone() is not None:
+            return {"error":"That username is already taken!"}
+        db.execute("INSERT (username, password, birthdate) INTO UserIndex(?, ?, ?)", (username, generate_password_hash(password), birthday))
+        db.commit()
+
+        user = db.execute("SELECT * FROM UserIndex WHERE username = ?", (username,)).fetchone()
+
+        return { "uid":str(user["id"]) }
