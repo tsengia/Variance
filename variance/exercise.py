b7bd05581e42096a4548c64d55c486c0114cef36
ExpandingDev <javscripter@yahoo.com>
2021-01-03 14:12:33 -0500

    Removing moved files

--- variance/exercise.py
+++
@@ -1,8 +0,0 @@
-from variance.anatomy import *
-
-# Stores information about the exercise, equipment used, muscles worked, 
-class Exercise():
-    def __init__(name, muscles):
-        self.name = name
-        self.muscles = muscles
-
