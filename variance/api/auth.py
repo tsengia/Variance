abf954d7a57e66c7688e154faac439eb14f31d3e
ExpandingDev <javscripter@yahoo.com>
2021-01-03 01:41:06 -0500

    Adding housekeeping commands

--- variance/api/auth.py
+++ variance/api/auth.py
@@ -1,7 +1,7 @@
 import functools
 import datetime
 import jwt
-
+import click
 from flask import (
     current_app, Blueprint, flash, g, redirect, request, session, url_for
 )
@@ -10,6 +10,38 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from variance.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/api/auth')
+
+@bp.cli.command("adduser")
+@click.argument("username")
+@click.argument("password")
+@click.argument("birthday")
+@click.option("-r","--role", "role", default=0, type=int)
+def cli_create_user(username, password, birthday, role):
+    db = get_db()
+    if db.execute("SELECT id FROM UserIndex WHERE id=?", (username,)).fetchone() is not None:
+        click.echo("A user with this username already exists!")
+        return -1
+    try:
+        birthday = datetime.date.fromisoformat(birthday)
+    except ValueError:
+        click.echo("The birthday must be in YYYY-MM-DD format!")
+    db.execute("INSERT INTO UserIndex (username, password, birthdate, role) VALUES (?, ?, ?, ?)", (username, generate_password_hash(password), birthday, role))
+    db.commit()
+    click.echo("User created.")
+
+@bp.cli.command("deluser")
+@click.argument("username")
+def cli_delete_user(username):
+    db = get_db()
+    
+    if db.execute("SELECT id FROM UserIndex WHERE username=?", (username,)).fetchone() is None:
+        click.echo("No user found.")
+        return -1
+
+    db.execute("DELETE FROM UserIndex WHERE username=?", (username,))
+    db.commit()
+    
+    click.echo("User deleted.")
 
 @bp.route("/login", methods=["POST"])
 def login():
@@ -76,8 +108,6 @@ def load_logged_in_user():
                 current_app.logger.warn("User attempted to use an invalid token!")
     if user_id is not None:
         g.user = get_db().execute("SELECT * FROM UserIndex WHERE id=?", (user_id,))
-
-
 
 def login_required(view):
     @functools.wraps(view)
