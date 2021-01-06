4be8381aa4479aab2e963c1b44b9802a3a13295f
ExpandingDev <tylersengia@gmail.com>
2021-01-05 23:11:01 -0500

    Moved config.py and added smorest API settings

--- variance/config.py
+++ variance/config.py
@@ -3,13 +3,16 @@ from os import environ as e
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+load_dotenv(path.join(basedir, '../.env'))
 
 class Config():
     SESSION_COOKIES_NAME = e.get("SESSION_COOKIE_NAME")
     STATIC_FOLDER = "static"
     TEMPLATES_FOLDER = "templates"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    API_TITLE = "VarianceAPI"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0"
 
 class ProdConfig(Config):
     SECRET_KEY = e.get("SECRET_KEY")
