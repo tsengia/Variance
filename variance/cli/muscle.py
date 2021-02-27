ed9c49dd43d011b80923827a314471a89cb5722e
ExpandingDev <tylersengia@gmail.com>
2021-02-27 17:16:13 -0500

    Added command to list muscle groups

--- variance/cli/muscle.py
+++ variance/cli/muscle.py
@@ -5,8 +5,10 @@ from variance import db
 from variance.models.muscle import MuscleModel, MuscleGroupModel
 
 muscle_cli = AppGroup("muscle")
+muscle_group_cli = AppGroup("group")
 muscle_mod_cli = AppGroup("mod")
 muscle_cli.add_command(muscle_mod_cli)
+muscle_cli.add_command(muscle_group_cli)
     
 @muscle_cli.command("list")
 def cli_muscle_list():
@@ -16,3 +18,12 @@ def cli_muscle_list():
         return -1
     for m in m_list:
         click.echo(str(m))
+        
+@muscle_group_cli.command("list")
+def cli_muscle_group_list():
+    mg_list = MuscleGroupModel.query.all()
+    if mg_list is None:
+        click.echo("MuscleGroup list is empty!")
+        return -1
+    for g in mg_list:
+        click.echo(str(g))
