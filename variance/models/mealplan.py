a084496714f77f10cb128648437514b4dfe29439
ExpandingDev <tylersengia@gmail.com>
2021-03-13 17:36:47 -0500

    Added ConsumedEntryModel

--- variance/models/mealplan.py
+++ variance/models/mealplan.py
@@ -1,6 +1,50 @@
 from datetime import datetime
 from variance import db
 
+class ConsumedEntryModel(db.Model):
+    __tablename__ = "ConsumedEntryIndex"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    
+    # Name of the food that was consumed.
+    name = db.Column(db.String(60), nullable=False)
+    
+    # Time when this entry was made
+    time = db.Column(db.DateTime(), nullable=False, default=datetime.now())
+    
+    # The user who added this mealplan to the database
+    owner_id = db.Column(db.Integer, db.ForeignKey("UserIndex.id"), nullable=False)
+    owner = db.relationship("UserModel", back_populates="consumption_entries")
+    
+    ### Nutritional Info
+    # Amount of calories consumed (grams)
+    calories = db.Column(db.Float, nullable=False, default=0)
+
+    # Amount of protein consumed (grams)
+    protein = db.Column(db.Float, nullable=False, default=0)
+
+    # Amount of carbohydrates consumed (grams)
+    carbohydrates = db.Column(db.Float, nullable=False, default=0)
+
+    # Amount of fats consumed (grams)
+    fat = db.Column(db.Float, nullable=False, default=0)
+    
+    ### Linking to a Consumable
+    servings = db.Column(db.Float, nullable=False, default=1)
+    consumable_id = db.Column(db.Integer, db.ForeignKey("ConsumableIndex.id"))
+    
+    consumable = db.relationship("ConsumableModel")
+
+    @staticmethod
+    def has_owner():
+        return True
+        
+    def check_owner(self, id):
+        return self.owner_id == id
+        
+    def __str__(self):
+        return "%u ConsumedEntryModel: %s @ @s, o(%u), c(%u)" % (self.id, self.name, str(self.time), self.owner_id, self.consumable_id)
+
 class MealPlanModel(db.Model):
     __tablename__ = "MealPlanIndex"
 
