36ba67a8681233b7c2ab1bc5fcc515429d3ad72a
ExpandingDev <tylersengia@gmail.com>
2021-05-15 22:04:27 -0400

    Fixing incorrect model names and logging

--- variance/models/lambda_measure.py
+++ variance/models/lambda_measure.py
@@ -1,20 +1,20 @@
 from variance import db 
 from sqlalchemy import select
-import logging
+import logging as logger
 
 # This is for dynamically calculated measures.
 # For example: 90% of 1 rep max, or 1/2 the pace of the PR time, etc.
 class LambdaModel(db.Model):
     __tablename__ = "LambdaIndex"
-    
+
     id = db.Column(db.Integer, primary_key=True)
 
     # Name of this lambda function, for example: "Percentage of 1 Rep Max"
     name = db.Column(db.String(100), nullable=False)
-    
+
     # Callable/internal name of this lambda function, for example "percent_1rm"
     function_name = db.Column(db.String(40), nullable=False)
-    
+
     def __str__(self):
         return "LambdaModel (%i): %s - %s" % (self.id, self.name, self.function_name)
 
@@ -26,7 +26,7 @@ def variance_evaluate_lambda(lambda_model, dimension, user_model, float_param, t
             logger.getLogger("variance").warning("Lambda evaluated missing a parameter!")
             return None
         
-        latest = select(TrackerEntry).where(TrackerEntry.parent_tracker_id == tracker_param.id).order_by(TrackerEntry.time).first()
+        latest = select(TrackerEntryModel).where(TrackerEntryModel.parent_tracker_id == tracker_param.id).order_by(TrackerEntry.time).first()
         if latest is None:
             ### TODO: Log this error
             return None
