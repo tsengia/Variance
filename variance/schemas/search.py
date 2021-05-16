42622bede548295b81922851c674f98fe25615d7
ExpandingDev <tylersengia@gmail.com>
2021-05-15 23:16:26 -0400

    Ran autopep8 -aaa

--- variance/schemas/search.py
+++ variance/schemas/search.py
@@ -5,7 +5,8 @@ class SearchSchema(ma.Schema):
     count = ma.fields.Integer(required=False, validate=ma.validate.Range(
         0, 225), default=99999, missing=99999)
     offset = ma.fields.Integer(
-        required=False, validate=ma.validate.Range(0, 255), default=0, missing=0)
+        required=False, validate=ma.validate.Range(
+            0, 255), default=0, missing=0)
 
     class Meta():
         unknown = ma.EXCLUDE
