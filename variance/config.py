a6ff8efa2c3ddb3a2760b39a0cfe88bfadfb29f2
ExpandingDev <tylersengia@gmail.com>
2021-05-15 23:19:21 -0400

    Ran autopep8 -aaa

--- variance/config.py
+++ variance/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '../.env'))
 
+
 class Config():
     SESSION_COOKIES_NAME = e.get("SESSION_COOKIE_NAME")
     STATIC_FOLDER = "static"
@@ -14,21 +15,24 @@ class Config():
     API_VERSION = "v1"
     OPENAPI_VERSION = "3.0"
 
+
 class ProdConfig(Config):
     SECRET_KEY = e.get("SECRET_KEY")
-    FLASK_ENV="production"
-    DEBUG=False
-    TESTING=False
-    SQLALCHEMY_DATABASE_URI=e.get("PROD_DATABASE_URI")
+    FLASK_ENV = "production"
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = e.get("PROD_DATABASE_URI")
+
 
 class DevConfig(Config):
     SECRET_KEY = "DEVELOPMENT KEY"
-    FLASK_ENV="development"
-    DEBUG=True
-    TESTING=True
-    SQLALCHEMY_DATABASE_URI=e.get("DEV_DATABASE_URI")
-    SQLALCHEMY_ECHO=True
+    FLASK_ENV = "development"
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = e.get("DEV_DATABASE_URI")
+    SQLALCHEMY_ECHO = True
+
 
 class UnitTestConfig(DevConfig):
-    SQLALCHEMY_DATABASE_URI=e.get("UNIT_TEST_DATABASE_URI")
-    SQLALCHEMY_ECHO=False
+    SQLALCHEMY_DATABASE_URI = e.get("UNIT_TEST_DATABASE_URI")
+    SQLALCHEMY_ECHO = False
