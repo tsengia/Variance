7bb54fb992271d3b1260f03093f4a956690dc7a9
ExpandingDev <tylersengia@gmail.com>
2021-05-15 23:35:15 -0400

    Fixed duplicate declaration of __str__

--- variance/models/exercise.py
+++ variance/models/exercise.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from variance import db
 
 
@@ -56,12 +54,9 @@ class ExerciseModel(db.Model):
     variations = db.relationship("ExerciseModel")
 
     def __str__(self):
-        return "%u Exercise: %s dur(%s), dis($s), wght(%s), equip(%s)" % (self.id, self.name, str(
+        return "%u Exercise: %s dur(%s), dis(%s), wght(%s), equip(%s)" % (self.id, self.name, str(
             self.use_duration), str(self.use_distance), str(self.use_weight), str(self.equipment.name))
 
     @staticmethod
     def has_owner():
         return False
-
-    def __str__(self):
-        return "%u ExerciseModel: %s" % (self.id, self.name)
