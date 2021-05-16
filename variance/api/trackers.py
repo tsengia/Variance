724db437c9f58584364c324a8171f64eb9471749
ExpandingDev <tylersengia@gmail.com>
2021-05-15 22:09:24 -0400

    Fixing incorrect import

---
+++ variance/api/trackers.py
@@ -0,0 +1,12 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from variance.models.tracker import TrackerModel, TrackerEntryModel
+
+class TrackerSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = TrackerModel
+        load_instance = False
+
+class TrackerEntrySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = TrackerEntryModel
+        load_instance = False
