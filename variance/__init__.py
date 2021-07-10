9eb02c29f1a5a34cfd8e99e3bb7e40136b9f79d3
Tyler Sengia <tylersengia@gmail.com>
2021-07-09 23:53:17 -0400

    redoing app factory

--- variance/__init__.py
+++ variance/__init__.py
@@ -7,23 +7,16 @@ from flask_smorest import Api
 
 db = SQLAlchemy()
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=False)
-
-    logging.basicConfig(
-        filename='variance.log',
-        level=logging.DEBUG,
-        format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-
-    if test_config is None:
-        app.config.from_object("variance.config.DevConfig")
-    else:
-        app.config.from_object(test_config)
-
-    logging.info("Variance configuration loaded.")
-
-    # Setup the instance directory structure
+def load_default_settings(app):
+    from variance.api.defaults import DefaultSettingsManager
     instance_path = pathlib.Path(app.instance_path)
+    defaults_path = instance_path / "defaults"
+    app.defaults_manager = DefaultSettingsManager(defaults_path)
+    logging.info("Default settings loaded.")
+
+def setup_instance_path(instance_path):
+    # Setup the instance directory structure
+    instance_path = pathlib.Path(instance_path)
     defaults_path = instance_path / "defaults"
     if not instance_path.exists():
         instance_path.mkdir(exist_ok=True)
@@ -35,13 +28,13 @@ def create_app(test_config=None):
             print("Defaults settings directory could not be created! Exiting.")
             exit()
         shutil.copyfile(str(pathlib.Path("variance") / "defaults" / "user.json"), str(defaults_path / "user.json")) 
+        logging.info("Created new instance directory")
 
-
-    rest_api = Api(app)
-
+def load_models(app):
     from variance.models import unit, muscle, equipment, exercise, gym, tracker, user, lambda_measure, permissions, workout, nutrition, mealplan
     logging.info("Variance Models imported.")
-    
+
+def load_cli(app):
     from variance import cli
     app.cli.add_command(cli.db.db_cli)
     app.cli.add_command(cli.permissions.permissions_cli)
@@ -61,23 +54,8 @@ def create_app(test_config=None):
     """
 
     logging.info("Variance CLI loaded.")
-    
-    from variance import api
-    app.register_blueprint(api.auth.bp, url_prefix="/api/auth")
-    app.register_blueprint(api.units.bp, url_prefix="/api/units")
 
-    logging.info("Variance blueprints loaded.")
-
-    db.init_app(app)
-
-    logging.info("Variance AppDB init done.")
- 
-    from variance.api.defaults import DefaultSettingsManager
-    @app.before_first_request
-    def load_default_settings():
-        app.defaults_manager = DefaultSettingsManager(defaults_path)
-        logging.info("Default settings loaded.")
-    
+def load_api(app):
     @app.route("/api/apiversion")
     def api_verison():
         return {"apiversion": "0.1"}
@@ -85,6 +63,46 @@ def create_app(test_config=None):
     @app.route("/api/version")
     def version():
         return {"version": "0.0.1 alpha"}
+    
+    from variance import api
+    app.register_blueprint(api.auth.bp, url_prefix="/api/auth")
+    app.register_blueprint(api.units.bp, url_prefix="/api/units")
+
+    logging.info("Variance API blueprints loaded.")
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=False)
+
+    logging.basicConfig(
+        filename='variance.log',
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
+    if test_config is None:
+        app.config.from_object("variance.config.DevConfig")
+    else:
+        app.config.from_object(test_config)
+
+    logging.info("Variance configuration loaded.")
+
+    setup_instance_path(app.instance_path)
+
+    rest_api = Api(app)
+
+    load_models(app)
+
+    load_cli(app)
+
+    load_api(app)
+
+    db.init_app(app)
+
+    logging.info("Variance AppDB init done.")
+ 
+    @app.before_first_request
+    def load_def_settings():
+        load_default_settings(app)
+
 
     return app
 
