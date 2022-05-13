c230de8626a81fe20b81a2ee1c16e54394b02e6d
ExpandingDev <tylersengia@gmail.com>
2022-05-12 23:39:04 -0400

    Fixing up unit schemas

--- variance/schemas/unit.py
+++ variance/schemas/unit.py
@@ -1,8 +1,14 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from variance.models.unit import UnitModel
+from marshmallow import Schema, fields, ValidationError, validates
 
+import re
 
-class UnitSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = UnitModel
-        load_instance = False
+unit_name_schema = re.compile("^[a-zA-Z0-9_-]{1,21}$")
+
+class UnitSchema(Schema):
+    name = fields.String(required=True)
+    abbreviation = fields.String(required=True)
+    dimension = fields.String(required=True)
+    multiplier = fields.Float(required=True)
+
+    # TODO: Add validator methods to check name, abbreviation, and dimension
+    # TODO: Add load_only and dump_only fields.
