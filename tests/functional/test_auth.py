199e138b6bb63d08a31c29cc176ccec48efe2430
ExpandingDev <tylersengia@gmail.com>
2022-05-21 12:23:11 -0400

    Adding more unit test cases

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -19,3 +19,73 @@ def test_bad_birthdate_registration(app_with_defaults, client):
                     })
     assert r.status_code == 422
     assert "status" in r.get_json()
+
+def test_short_password_registration(app_with_defaults, client):
+    r = client.post("/api/auth/register",
+                    data={
+                        "username": "shortpassword",
+                        "password": "123",
+                        "birthdate": "2014-03-10"
+                    })
+    assert r.status_code == 422
+    assert "message" in r.get_json()
+
+def test_bad_username_registration(app_with_defaults, client):
+    r = client.post("/api/auth/register",
+                    data={
+                        "username": "oh",
+                        "password": "123456789",
+                        "birthdate": "2014-03-10"
+                    })
+    assert r.status_code == 422
+    assert "message" in r.get_json()
+
+    r = client.post("/api/auth/register",
+                    data={
+                        "username": "abcdefgh123[]oops",
+                        "password": "123456789",
+                        "birthdate": "2014-03-10"
+                    })
+    assert r.status_code == 422
+    assert "message" in r.get_json()
+
+    r = client.post("/api/auth/register",
+                    data={
+                        "username": "ThisIsALongUsernameThatIsMoreThan21Chars",
+                        "password": "123456789",
+                        "birthdate": "2014-03-10"
+                    })
+    assert r.status_code == 422
+    assert "message" in r.get_json()
+
+
+def test_duplicate_registration(app_with_defaults, client):
+    # First username, should succeed
+    r = client.post("/api/auth/register",
+                    data={
+                        "username": "test4",
+                        "password": "asdfghjkl",
+                        "birthdate": "2014-03-10"
+                    })
+    assert r.status_code == 201
+    assert "id" in r.get_json()
+
+    # Duplicate username, should error out
+    r = client.post("/api/auth/register",
+                    data={
+                        "username": "test4",
+                        "password": "asdfghjkl",
+                        "birthdate": "2014-03-10"
+                    })
+    assert r.status_code == 409
+    assert "message" in r.get_json()
+
+    # Another duplicate username, should error out
+    r = client.post("/api/auth/register",
+                    data={
+                        "username": "test4",
+                        "password": "ery3456i",
+                        "birthdate": "2011-02-10"
+                    })
+    assert r.status_code == 409
+    assert "message" in r.get_json()
--- variance/schemas/auth.py
+++ variance/schemas/auth.py
@@ -1,5 +1,6 @@
 from marshmallow import fields, Schema, ValidationError, validates
 from variance.schemas.user import username_regex
+from flask_smorest import abort
 
 class TokenAuthSchema(Schema):
     token = fields.String()
@@ -14,12 +15,12 @@ class RegisterSchema(Schema):
     def validate_new_username(self, value):
         # Make sure there are no illegal characters, meets length requirements, and is unique.
         if not username_regex.match(value):
-            raise ValidationError("Username must be between 3 and 21 characters and can only contain alphanumeric values!")
+            abort(422, message="Username must be between 3 and 21 characters and can only contain alphanumeric values!")
 
     @validates("password")
     def validate_new_password(self, value):
         if len(value) < 8:
-            raise ValidationError("Password must be at least 8 characters long!")
+            abort(422, message="Password must be at least 8 characters long!")
 
 
 class LoginSchema(Schema):
@@ -30,10 +31,10 @@ class LoginSchema(Schema):
     def validate_username(self, value):
         # By double checking that the username is legal, we save ourselves a hit to the database.
         if not username_regex.match(value):
-            raise ValidationError("Username must be between 3 and 21 characters and can only contain alphanumeric values!")
+            abort(422, message="Username must be between 3 and 21 characters and can only contain alphanumeric values!")
 
     @validates("password")
     def validate_password(self, value):
         # By checking the length of the password, we save ourselve a hit to the database.
         if len(value) < 8:
-            raise ValidationError("Password must be at least 8 characters long!")
+            abort(422, message="Password must be at least 8 characters long!")
