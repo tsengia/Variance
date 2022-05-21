c03718bef9312001169b7507e71cc35891a7ac83
ExpandingDev <tylersengia@gmail.com>
2022-05-21 19:58:23 -0400

    Added StatusSchema

--- variance/schemas/unit.py
+++ variance/schemas/unit.py
@@ -1,15 +1,5 @@
 from marshmallow import Schema, fields, ValidationError, validates
 
-import re
-
-unit_name_schema = re.compile("^[a-zA-Z0-9_-]{1,21}$")
-
-class UnitSchema(Schema):
-    id = fields.Integer(required=True)
-    name = fields.String(required=True)
-    abbreviation = fields.String(required=True)
-    dimension = fields.String(required=True)
-    multiplier = fields.Float(required=True)
-
-    # TODO: Add validator methods to check name, abbreviation, and dimension
-    # TODO: Add load_only and dump_only fields.
+class StatusSchema(Schema):
+    status = fields.String(required=True)
+    code = fields.Integer(required=True)
