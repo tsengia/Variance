be3944e7ea73fab482c02d8ea1b11dd81d2b4d03
ExpandingDev <tylersengia@gmail.com>
2022-05-21 23:08:27 -0400

    Added JSON dumping of muscles

--- variance/cli/export_data.py
+++ variance/cli/export_data.py
@@ -7,6 +7,10 @@ from variance.extensions import db
 from variance.models.unit import UnitModel
 from variance.schemas.unit import UnitSchema
 
+from variance.models.muscle import MuscleModel
+from variance.schemas.muscle import MuscleSchema
+
+
 export_cli = AppGroup("export")
 
 @export_cli.command("all")
@@ -21,19 +25,29 @@ def cli_export_list():
         click.echo("Unit list is empty!")
         return -1
     export_dir = Path("exported")
+    export_dir.mkdir(exist_ok=True)
     unit_export_dir = export_dir / "units"
-    unit_export_dir.mkdir(parents=True)
+    unit_export_dir.mkdir()
     unit_dump_schema = UnitSchema(exclude=("id",))
     for u in u_list:
         cname = u.canonical_name
         unit_file = unit_export_dir / (cname + ".json")
         unit_file.write_text(str(unit_dump_schema.dump(u)))
 
-#@export_cli.command("view")
-#@click.argument("id")
-#def cli_export_view(id):
-#    u = UnitModel.query.get(id)
-#    if u is None:
-#        click.echo("Could not find an export with that ID!")
-#        return -1
-#    click.echo(str(u))
+
+@export_cli.command("muscles")
+def cli_export_list():
+    m_list = MuscleModel.query.all()
+    if m_list is None:
+        click.echo("Muscle list is empty!")
+        return -1
+    export_dir = Path("exported")
+    export_dir.mkdir(exist_ok=True)
+    muscle_export_dir = export_dir / "muscles"
+    muscle_export_dir.mkdir()
+    muscle_dump_schema = MuscleSchema(exclude=("id",))
+    for m in m_list:
+        cname = m.canonical_name
+        muscle_file = muscle_export_dir / (cname + ".json")
+        muscle_file.write_text(str(muscle_dump_schema.dump(m)))
+
