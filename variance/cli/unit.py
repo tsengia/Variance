a4b693980ec2b0fbc3eca47695f6574a7b0119ea
ExpandingDev <tylersengia@gmail.com>
2022-06-11 16:08:18 -0400

    Moving unit exporting to unit.py cli file

--- variance/cli/unit.py
+++ variance/cli/unit.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 import click
 from flask.cli import AppGroup
 
 from variance.extensions import db
 from variance.models.unit import UnitModel
+from variance.schemas.unit import UnitSchema
 
 unit_cli = AppGroup("unit")
 unit_mod_cli = AppGroup("mod")
@@ -34,3 +36,19 @@ def count_units():
 @unit_cli.command("count")
 def cli_unit_count():
     click.echo("There are {i} units in the database.".format(i=count_units()))
+
+@unit_cli.command("export")
+def cli_unit_export():
+    u_list = UnitModel.query.all()
+    if u_list is None:
+        click.echo("Unit list is empty!")
+        return -1
+    export_dir = Path("exported")
+    export_dir.mkdir(exist_ok=True)
+    unit_export_dir = export_dir / "units"
+    unit_export_dir.mkdir()
+    unit_dump_schema = UnitSchema(exclude=("id",))
+    for u in u_list:
+        cname = u.canonical_name
+        unit_file = unit_export_dir / (cname + ".json")
+        unit_file.write_text(unit_dump_schema.dumps(u))
