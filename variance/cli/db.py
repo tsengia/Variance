391d153276242cf0a0d78c7600c02274ef496af2
ExpandingDev <tylersengia@gmail.com>
2022-06-11 00:39:11 -0400

    Added flask db prompt for interactive db SQL queries

--- variance/cli/db.py
+++ variance/cli/db.py
@@ -2,6 +2,8 @@ import click
 from flask.cli import AppGroup
 from variance.extensions import db
 
+from sqlalchemy import text
+
 db_cli = AppGroup("db")
 
 
@@ -17,3 +19,37 @@ def cli_db_init():
     click.echo("Initializing database...")
     db.create_all()
     click.echo("Database initialized.")
+
+@db_cli.command("prompt")
+def cli_db_prompt():
+    click.echo("Entering Database interactive prompt, be careful!")
+    done = False
+    with db.session() as s:
+        try:
+            while not done:
+                cmd = click.prompt("VDB")
+                if (cmd.lower() == "quit" or cmd.lower() == "exit"):
+                    done = True
+                    break 
+                elif (cmd.lower() == "commit"):
+                    s.commit()
+                elif cmd.lower() == "rollback":
+                    s.rollback()
+                else:
+                    res = s.execute(text(cmd))
+                    rows = res.all()
+                    click.echo("Command returned {i} rows.".format(i=len(rows)))
+                    if len(rows) > 0:
+                        key_string = "Keys: "
+                        keys = res.keys()
+                        for k in keys:
+                            key_string += k + ", "
+                        click.echo(key_string)
+                    for r in rows:
+                        click.echo(str(r).replace("(","").replace(")",""))
+        except Exception as e:
+            click.echo("Exception caught, rollingback and exiting!")
+            click.echo("Exception: " + str(e))
+            s.rollback()
+    
+    click.echo("Exited Database interactive prompt.")
