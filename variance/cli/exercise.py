d46aee76e4b3fc42d837df76364e3e337b009bab
ExpandingDev <tylersengia@gmail.com>
2022-06-11 16:18:51 -0400

    Moving export commands

--- variance/cli/exercise.py
+++ variance/cli/exercise.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 import click
 from flask.cli import AppGroup
 
 from variance.extensions import db
 from variance.models.exercise import ExerciseModel
+from variance.schemas.exercise import ExerciseSchema
 
 exercise_cli = AppGroup("exercise")
 exercise_mod_cli = AppGroup("mod")
@@ -30,3 +32,19 @@ def cli_exercise_view(id):
     click.echo("\t'%s'" % (e.description))
     for q in e.equipment:
         click.echo("\t-> %u %s" % (q.id, q.name))
+
+@exercise_cli.command("export")
+def cli_exercise_export():
+    e_list = ExerciseModel.query.all()
+    if e_list is None:
+        click.echo("Exercise list is empty!")
+        return -1
+    export_dir = Path("exported")
+    export_dir.mkdir(exist_ok=True)
+    exercise_export_dir = export_dir / "exercises"
+    exercise_export_dir.mkdir()
+    exercise_dump_schema = ExerciseSchema(exclude=("id",))
+    for e in e_list:
+        cname = e.canonical_name
+        exercise_file = exercise_export_dir / (cname + ".json")
+        exercise_file.write_text(exercise_dump_schema.dumps(e))
--- variance/cli/nutrient.py
+++ variance/cli/nutrient.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 import click
 from flask.cli import AppGroup
 
 from variance.extensions import db
 from variance.models.nutrition import NutrientInfoModel
+from variance.schemas.nutrition import NutrientInfoSchema
 
 nutrient_cli = AppGroup("nutrient")
 nutrient_mod_cli = AppGroup("mod")
@@ -27,3 +29,19 @@ def cli_nutrient_view(id):
         click.echo("Could not find an nutrient with that ID!")
         return -1
     click.echo(str(n))
+
+@nutrient_cli.command("export")
+def cli_nutrient_export():
+    n_list = NutrientInfoModel.query.all()
+    if n_list is None:
+        click.echo("NutrientInfo list is empty!")
+        return -1
+    export_dir = Path("exported")
+    export_dir.mkdir(exist_ok=True)
+    nutrient_export_dir = export_dir / "nutrients"
+    nutrient_export_dir.mkdir()
+    nutrient_dump_schema = NutrientInfoSchema(exclude=("id",))
+    for n in n_list:
+        cname = n.canonical_name
+        nutrient_file = nutrient_export_dir / (cname + ".json")
+        nutrient_file.write_text(nutrient_dump_schema.dumps(n))
