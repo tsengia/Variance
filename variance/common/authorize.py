6d153d8e7c569cc85c6f5b87f5f33d00ee458e16
ExpandingDev <tylersengia@gmail.com>
2022-06-14 20:19:45 -0400

    Moving authorization and uniqueness validation to seperate files

---
+++ variance/common/authorize.py
@@ -0,0 +1,39 @@
+from flask_smorest import abort
+from marshmallow import Schema, validates, ValidationError, fields
+
+from variance.models.permissions import PermissionModel
+
+# Internal function that checks to see if there are any permission entries
+# that permit the user/client to perform the action
+def check_perms(user, action, model):
+    """
+    Checks the permission tables to see if the given user is authorized
+    to perform the given action.
+
+    If the user has no identity (ie. not logged in), then user can be
+    set to False.
+    The model that the user is performing the action is needed to check
+    ownership.
+
+    Returns True if the user is authorized to perform the action on the
+    given model, False otherwise.
+    """
+    a = PermissionModel.query.filter_by(action=action)
+    for p in a:
+        if p.check_user(user, model):
+            return True
+    # TODO: Make log to notify dev if no perm entries for that action can be
+    # found
+    return False
+
+
+def check_perms_or_abort(user, action, model):
+    """
+    Calls check_perms() and aborts with a 401 message if the user is not
+    authorized to perform the action on the given model.
+    """
+    allowed = check_perms(user, action, model)
+
+    if not allowed:
+        # TODO: Log an error
+        abort(401, message={"error": "You do not have permission to perform this action!"})
---
+++ variance/common/util.py
@@ -0,0 +1,20 @@
+from flask_smorest import abort
+
+def validate_unique(value, model, field, errmsg):
+    """
+    Checks to see if there are any instances of `model` in the database that
+    have `field_name` set to `value`.
+    Example: Checking is any UserModel's have their username set to "new_username".
+
+    Returns True if unique, False otherwise.
+    """
+    return not (model.query.filter(field == value).first() is not None)
+
+def validate_unique_or_abort(value, model, field, errmsg):
+    """
+    Calls validate_unique() and aborts with a 409 message if there is an
+    already existing model that has the given field set to the same value.
+    """
+    unique = not (model.query.filter(field == value).first() is not None)
+    if not unique:
+        abort(409, message=errmsg)
