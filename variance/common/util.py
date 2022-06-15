c49117cd2c3d3513ebb7f6c852b8a8763f6f889c
ExpandingDev <tylersengia@gmail.com>
2022-06-14 21:13:07 -0400

    Adding canonize to util.py

--- variance/common/util.py
+++ variance/common/util.py
@@ -1,5 +1,17 @@
 from flask_smorest import abort
 
+def canonize(str: name):
+    """
+    Helper function that transforms a display name into a canonical name.
+    """
+    return name.lower()\
+        .replace("/"," per ")\
+        .replace("&", " and ")\
+        .replace("+", " and ")\
+        .replace(" ", "-")\
+        .replace("_","-")
+
+
 def validate_unique(value, model, field, errmsg):
     """
     Checks to see if there are any instances of `model` in the database that
@@ -9,6 +21,7 @@ def validate_unique(value, model, field, errmsg):
     Returns True if unique, False otherwise.
     """
     return not (model.query.filter(field == value).first() is not None)
+
 
 def validate_unique_or_abort(value, model, field, errmsg):
     """
