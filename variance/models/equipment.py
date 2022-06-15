803f668a3a2df9b1ab5bf1d64f83e1aae78876ba
ExpandingDev <tylersengia@gmail.com>
2022-06-14 22:47:55 -0400

    Expanding max length constrain for equipment description

--- variance/models/equipment.py
+++ variance/models/equipment.py
@@ -27,7 +27,7 @@ class EquipmentModel(db.Model):
     name = db.Column(db.String(40), unique=True, nullable=False)
 
     # Describe this piece of equipment.
-    description = db.Column(db.String(20), nullable=True)
+    description = db.Column(db.String(200), nullable=True)
 
     # List of exercises that use this piece of equipment.
     exercises = db.relationship(
