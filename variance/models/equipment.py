b7dced92415c37a601942b269189631a6ffb7c9c
ExpandingDev <tylersengia@gmail.com>
2022-06-14 21:08:19 -0400

    Adding class docstring for EquipmentModel

--- variance/models/equipment.py
+++ variance/models/equipment.py
@@ -2,6 +2,20 @@ from variance.extensions import db
 
 
 class EquipmentModel(db.Model):
+    """
+    Model for a piece of exercise equipment.
+    Examples: Dumbbells, Barbell, Treadmill, Bench.
+
+    Instances are stored in the `EquipmentIndex` table.
+
+    Attributes:
+        id - Unique integer ID, primary key
+        canonical_name - Unique string used during import, export, and linking
+        name - Non-unique display string for users to see
+        description - String explaining what this piece of equipment is
+        exercises - List of ExerciseModels that use this piece of equipment
+    """
+
     __tablename__ = "EquipmentIndex"
 
     id = db.Column(db.Integer, primary_key=True)
