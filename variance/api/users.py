74cea9e28343ac126fb50a3c7756ff052b03329d
ExpandingDev <tylersengia@gmail.com>
2022-06-15 19:26:01 -0400

    Removing old code

--- variance/api/users.py
+++
@@ -1,8 +0,0 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from variance.models.user import UserModel
-
-
-class UserSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = UserModel
-        load_instance = False
