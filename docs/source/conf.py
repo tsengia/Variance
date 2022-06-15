b92ab3ca353a17ef3d00c772afdfaea2d1df38af
ExpandingDev <tylersengia@gmail.com>
2022-06-15 19:14:48 -0400

    Adding docs autogeneration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -28,8 +28,12 @@ author = 'Tyler Sengia'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary"
 ]
-
+autosummary_generate = True
+autosummary_imported_members = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,16 +1,12 @@
-.. Variance documentation master file, created by
-   sphinx-quickstart on Wed Jun 15 17:05:20 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 
 Welcome to Variance's documentation!
 ====================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 3
    :caption: Contents:
 
-
+   api
 
 Indices and tables
 ==================
