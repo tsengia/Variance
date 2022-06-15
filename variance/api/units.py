4c97d0bbd5d44d6df43aad6e9d934b26ffe7055a
ExpandingDev <tylersengia@gmail.com>
2022-06-14 20:22:16 -0400

    Changing to common.util and common.authorize

--- variance/api/units.py
+++ variance/api/units.py
@@ -2,7 +2,8 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from variance.extensions import db
-from variance.util import check_perms, validate_unique
+from variance.common.util import validation_unique_or_abort
+from variance.common.authorize import check_perms_or_abort
 from variance.models.unit import UnitModel
 from variance.schemas.util import StatusSchema
 from variance.schemas.unit import UnitSchema, UnitIDSchema
@@ -17,7 +18,7 @@ class UnitList(MethodView):
     @bp.arguments(UnitSchema(), location="form", unknown=EXCLUDE)
     @bp.response(201, UnitIDSchema())
     def post(self, new_unit):  # Create a new unit
-        check_perms("unit.new", False)
+        check_perms_or_abort("unit.new", False)
         if UnitModel.query.filter_by(
                 name=new_unit["name"]).first() is not None:
             abort(409, message="A unit with that name already exists!")
@@ -31,7 +32,7 @@ class UnitList(MethodView):
                   location="query", required=False, unknown=EXCLUDE)
     @bp.response(200, UnitSchema(many=True))
     def get(self, search_args, unit_args):  # List all units
-        check_perms("unit.view", False)
+        check_perms_or_abort("unit.view", False)
         if "dimension" not in unit_args:
             result = UnitModel.query.limit(search_args["count"]).offset(
                 search_args["offset"]).all()
@@ -51,7 +52,7 @@ class Unit(MethodView):
     @bp.response(200, StatusSchema)
     def post(self, update, unit_id):  # Update a unit
         u = UnitModel.query.get_or_404(unit_id)
-        check_perms("unit.update", u)
+        check_perms_or_abort("unit.update", u)
         
         if "name" in update and update["name"] != u.name:
             if UnitModel.query.filter_by(name=update["name"]).count() != 0:
@@ -67,7 +68,7 @@ class Unit(MethodView):
     @bp.response(200, StatusSchema)
     def delete(self, unit_id):  # Delete a unit
         u = UnitModel.query.get_or_404(unit_id)
-        check_perms("unit.delete", u)
+        check_perms_or_abort("unit.delete", u)
 
         db.session.delete(u)
         db.session.commit()
@@ -77,5 +78,5 @@ class Unit(MethodView):
     @bp.response(200, UnitSchema)
     def get(self, unit_id):  # Display a unit
         u = UnitModel.query.get_or_404(unit_id)
-        check_perms("unit.view", u)
+        check_perms_or_abort("unit.view", u)
         return u
