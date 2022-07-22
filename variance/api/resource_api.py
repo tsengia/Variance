430064c012eade97468fdfadca4024ab4f01c499
ExpandingDev <tylersengia@gmail.com>
2022-07-21 23:00:06 -0400

    Working on API endpoint class

--- variance/api/resource_api.py
+++ variance/api/resource_api.py
@@ -20,9 +20,52 @@ class VarianceResource(resource_schema, resource_model, file_name: str, endpoint
     self._endpoint_name = endpoint_name
 
     @blueprint.route("/")
-    class Collection(MethodView):
-        @blueprint.arguments(resource_schema())
-        @blueprint.response(201, resource_schema())
-        def post(self, post_schema: resource_schema) -> resource_model:
-            "Endpoint for creating new instances of this type"
-            authorize_user_or_abort(False, self._resource_schema + ".new", resource_model)
+    class VarianceResourceCollectionEndpoint(MethodView):
+        def get(self):
+            # TODO: Implement this, include paging
+            pass
+
+        @blueprint.arguments(resource_schema(exclude=("id",))
+        @blueprint.response(200, resource_schema)
+        def post(self, new_resource: resource_schema) -> resource_model:
+            authorize_user_or_abort(g.user, endpoint_name + ".new", False)
+            # TODO: Enforcement of unique constraints
+            m = resource_model(**new_resource)
+            db.session.add(m)
+            db.session.commit()
+            pass
+
+    @blueprint.route("/<int:resource_id>")
+    class VarianceResourceEndpoint(MethodView):
+
+        @blueprint.response(200, resource_schema)
+        def get(self, resource_id: int) -> resource_model:
+            m = resource_model.query.get_or_404(resource_id)
+            authorize_user_or_abort(g.user, endpoint_name + ".view", m)
+            return m
+
+        @blueprint.arguments(resource_schema)
+        @blueprint.response(200, resource_schema)
+        def post(self, resource_patch: object, resource_id: int) -> resource_omdel:
+            m = resource_model.query.get_or_404(resource_id)
+            authorize_user_or_abort(g.user, endpoint_name + ".delete", m)
+          
+            # Check to make sure the user is not modifying the ID value
+            if resource_id != resource_patch.id:
+                abort(409, "You are not permitted to change the ID of a resource!")
+
+            # Apply the update
+            for key, value in resource_patch.items():
+                setattr(m, key, value)
+
+            # Commit the changes
+            db.session.commit()
+            return m 
+    
+        @blueprint.response(204)
+        def delete(self, resource_id: int):
+            m = resource_model.query.get_or_404(resource_id)
+            authorize_user_or_abort(g.user, endpoint_name + ".delete", m)
+            db.session.delete(m)
+            db.session.commit()
+            return
