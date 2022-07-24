6c1bc770ef626193af192ae911896949e7de259c
ExpandingDev <tylersengia@gmail.com>
2022-07-24 13:42:13 -0400

    Removing permissions schema

--- variance/schemas/permissions.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import fields
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from variance.models.permissions import PermissionModel
-
-
-class PermissionSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = PermissionModel
-        load_instance = False
