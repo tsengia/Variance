f8b5c2a14fb76149fde430d07b9a3fef39e05e71
ExpandingDev <tylersengia@gmail.com>
2022-07-24 17:12:51 -0400

    Adding muscle.uuid back to groups export

--- variance/cli/muscle.py
+++ variance/cli/muscle.py
@@ -3,5 +3,5 @@ from variance.schemas.muscle import MuscleSchema, MuscleGroupSchema
 from variance.cli.resource import ResourceCLI
 
 muscle_cli = ResourceCLI(MuscleModel, MuscleSchema, "Muscles", "muscles", ("groups",))
-muscle_group_cli = ResourceCLI(MuscleGroupModel, MuscleGroupSchema, "MuscleGroups", "groups", ("muscles.uuid",))
+muscle_group_cli = ResourceCLI(MuscleGroupModel, MuscleGroupSchema, "MuscleGroups", "groups")
 muscle_group_cli.attach(muscle_cli.group)
