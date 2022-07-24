45f6e5455c62b02801bc715a201ef33e550262da
ExpandingDev <tylersengia@gmail.com>
2022-07-24 13:43:31 -0400

    Making ResourceBaseModel

--- variance/extensions.py
+++ variance/extensions.py
@@ -1,3 +1,11 @@
+import uuid
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class ResourceBase(db.Model):
+    "Base model that all Resource models inherit from"
+    __abstract__ = True
+
+    uuid = db.Column(db.String(36), primary_key=True, unique=True, default=uuid.uuid4())
+    "UUID that serves as the primary key for the resource model"
