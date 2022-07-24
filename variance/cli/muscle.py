e0a20daca8c6f571dd2ee06cc5522840f172460b
ExpandingDev <tylersengia@gmail.com>
2022-07-24 14:27:02 -0400

    Working on cli

--- variance/cli/muscle.py
+++ variance/cli/muscle.py
@@ -2,6 +2,6 @@ from variance.models.muscle import MuscleModel, MuscleGroupModel
 from variance.schemas.muscle import MuscleSchema, MuscleGroupSchema
 from variance.cli.resource import ResourceCLI
 
-muscle_cli = ResourceCLI(MuscleModel, MuscleSchema, "Muscles", "muscles", ("id","groups"))
-muscle_group_cli = ResourceCLI(MuscleGroupModel, MuscleGroupSchema, "MuscleGroups", "groups", ("id", "muscles.id"))
+muscle_cli = ResourceCLI(MuscleModel, MuscleSchema, "Muscles", "muscles", ("groups",))
+muscle_group_cli = ResourceCLI(MuscleGroupModel, MuscleGroupSchema, "MuscleGroups", "groups", ("muscles.uuid",))
 muscle_group_cli.attach(muscle_cli.group)
