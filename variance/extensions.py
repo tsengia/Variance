d4787e2d6c86cae1b083dc6146108be5c26e9f19
ExpandingDev <tylersengia@gmail.com>
2022-07-24 14:12:10 -0400

    Fixing UUID gen

--- variance/extensions.py
+++ variance/extensions.py
@@ -7,5 +7,5 @@ class ResourceBase(db.Model):
     "Base model that all Resource models inherit from"
     __abstract__ = True
 
-    uuid = db.Column(db.String(36), primary_key=True, unique=True, default=uuid.uuid4())
+    uuid = db.Column(db.String(36), primary_key=True, unique=True, default=str(uuid.uuid4()))
     "UUID that serves as the primary key for the resource model"
